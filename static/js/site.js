2ee9b648b73c7047b51520aad6da0bd0d6e456a0
Matt Raibert <mattraibert@gmail.com>
2014-07-22 18:12:18 -0400

    Show a snappy preview of where the tutorial will be.

--- static/js/site.js
+++ static/js/site.js
@@ -7,17 +7,33 @@ $(function() {
       .attr("class", "tutorial")
       .attr("xlink:href", function(d) {return "/img/images.jpeg";})
       .attr("width",60).attr("height",60)
-      .attr("x", function(d) {return d.x * 100;})
-      .attr("y", function(d) {return d.y * 50;});
+      .attr("x", function(d) {return d.x * 100 + 20;})
+      .attr("y", function(d) {return d.y * 50 + 20;});
   });
 
   d3.select("svg")
     .on("click", function() {
       var click = d3.mouse(this);
       var x = Math.floor(click[0]/100);
-      var y = Math.floor(click[1]/50);
+      var y = Math.floor((click[1] - 25)/50);
+
       $.post("/tutorials", {"new.x": x , "new.y": y, "new.title": "nope"}, function() {
 	window.location.reload();
       });
-  });
+    })
+    .on("mousemove", function() {
+      var click = d3.mouse(this);
+      var x = Math.floor(click[0]/100);
+      var y = Math.floor((click[1] - 25)/50);
+
+      d3.select("svg").selectAll("image.feedback").remove();
+      d3.select("svg")
+	.append("image")
+	.attr("class", "feedback")
+	.attr("xlink:href", "/img/images.jpeg")
+	.attr("width", 60).attr("height", 60)
+	.attr("x", x * 100 + 20)
+	.attr("y", y * 50 + 20)
+	.attr("opacity", 0.5);
+    });
 });
