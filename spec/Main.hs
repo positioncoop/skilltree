fa5aceb77c9fb52680a2a85175025e4afd242524
Daniel Patterson <dbp@dbpmail.net>
2014-10-09 11:29:28 -0400

    WIP

--- spec/Main.hs
+++ spec/Main.hs
@@ -46,7 +46,7 @@ class Factory b a c d | a -> b, a -> c, a -> d, d -> a where
   reload = return
 
 
-type TutorialFields = (Int, Int, T.Text, Maybe FilePath, Publish)
+newtype TutorialFields = TutorialFields (Int, Int, T.Text, Maybe FilePath, Publish)
 
 instance Factory App TutorialEntity TutorialId TutorialFields where
   factoryFields = (0, 0, "Untitled", Nothing, Draft)
@@ -66,7 +66,7 @@ instance Factory App AuthUser A.UserId (IO T.Text) where
                    return (fromJust $ A.userId au')
   setId i a = a { A.userId = Just i }
 
-type DependentFields = (SnapHspecM App TutorialEntity, SnapHspecM App TutorialEntity)
+newtype DependentFields = DependentFields (SnapHspecM App TutorialEntity, SnapHspecM App TutorialEntity)
 
 instance Factory App DependencyEntity DependencyId DependentFields where
   factoryFields = (create (), create ())
@@ -80,14 +80,14 @@ instance Factory App DependencyEntity DependencyId DependentFields where
 type HspecApp = SnapHspecM App
 
 setTitle :: T.Text -> TutorialFields -> TutorialFields
-setTitle title (x, y, _, icon, publish) = (x, y, title, icon, publish)
+setTitle title (TutorialFields (x, y, _, icon, publish)) = TutorialFields (x, y, title, icon, publish)
 setMode :: Publish -> TutorialFields -> TutorialFields
-setMode mode (x, y, title, icon, _) = (x, y, title, icon, mode)
+setMode mode (TutorialFields (x, y, title, icon, _)) = TutorialFields (x, y, title, icon, mode)
 
 setDependent1 :: SnapHspecM App TutorialEntity -> DependentFields -> DependentFields
-setDependent1 l (_,r) = (l,r)
+setDependent1 l (DependentFields (_,r)) = DependentFields (l,r)
 setDependent2 :: SnapHspecM App TutorialEntity -> DependentFields -> DependentFields
-setDependent2 r (l,_) = (l,r)
+setDependent2 r (DependentFields (l,_)) = DependentFields (l,r)
 
 setDependentModes m1 m2 = setDependent1 (create (setMode m1)) . setDependent2 (create (setMode m2))
 
